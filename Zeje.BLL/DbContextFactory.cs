dbb2f30627e031ccb4fbdd367fdf33ed2e4a12fe
zeje <zezhi821@163.com>
2015-06-24 18:37:22 +0800

    test
    test

---
+++ Zeje.BLL/DbContextFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using EntityFramework;
+using Zeje.Model;
+
+namespace Zeje.BLL
+{
+    /// <summary>winform主线程为单线程，不能用
+    /// </summary>
+    public class DbContextFactory
+    {
+        /// <summary>保证了线程内DbContext实例唯一
+        /// </summary>
+        /// <returns></returns>
+        public static Model.Sys.SysEntities GetSysDbContext()
+        {
+            Model.Sys.SysEntities dbContext = CallContext.GetData("SysDbContext") as Model.Sys.SysEntities;
+            if (dbContext == null)
+            {
+                dbContext = new Model.Sys.SysEntities();
+                dbContext.Configuration.ProxyCreationEnabled = false;
+                CallContext.SetData("SysDbContext", dbContext);
+            }
+            return dbContext;
+        }
+
+        public static void RefreshDbContext()
+        {
+            CallContext.SetData("SysDbContext", null);
+        }
+    }
+}
---
+++ Zeje.Core/Expression/Predicate_.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Linq.Expressions;
+
+namespace Zeje.Core
+{
+    /// <summary>表达式谓语扩展
+    /// <remarks>
+    /// 1、构造函数使用True时：
+    /// （1）单个AND有效，多个AND有效，当然了True&&XX=XX
+    /// （2）单个OR无效，多个OR无效：True || XX=True所以无效。
+    /// （3）混合时：
+    ///     ※(True && XX) || XX1 等于 XX || XX1，有效；
+    ///     ※(True || XX) && XX1 等于 True && XX1 等于 XX1，局部有效
+    ///     ※(True || XX) || XX1 等于 True||XX1 等于 True，全部无效
+    /// 2、构造函数使用False时：和True情况一样 
+    /// </remarks>
+    /// </summary>
+    public static class Predicate_
+    {
+        /// <summary>True
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static Expression<Func<T, bool>> True<T>() { return f => true; }
+        /// <summary>False
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static Expression<Func<T, bool>> False<T>() { return f => false; }
+        /// <summary>Or 或
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="expression1"></param>
+        /// <param name="expression2"></param>
+        /// <returns></returns>
+        public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> expression1,
+           Expression<Func<T, bool>> expression2)
+        {
+            var invokedExpression = Expression.Invoke(expression2, expression1.Parameters
+                    .Cast<Expression>());
+
+            return Expression.Lambda<Func<T, bool>>(Expression.Or(expression1.Body, invokedExpression),
+            expression1.Parameters);
+        }
+        /// <summary>And 且
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="expression1"></param>
+        /// <param name="expression2"></param>
+        /// <returns></returns>
+        public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> expression1,
+              Expression<Func<T, bool>> expression2)
+        {
+            var invokedExpression = Expression.Invoke(expression2, expression1.Parameters
+                 .Cast<Expression>());
+
+            return Expression.Lambda<Func<T, bool>>(Expression.And(expression1.Body,
+                   invokedExpression), expression1.Parameters);
+        }
+    }
+}
